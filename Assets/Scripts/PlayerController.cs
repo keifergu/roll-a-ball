17f58c2720006c02d7864ea6796cc20a081017fb
Keifer Gu <keifergu@gmail.com>
2017-02-21 15:33:23 +0800

    添加碰撞检测； 添加收集物块； 添加计分文本； 添加胜利文本；

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
+
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -18,5 +25,20 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			SetCountText ();
+			count = count + 1;
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 11) {
+			winText.text = "You Win!";
+		}
 	}
 }
